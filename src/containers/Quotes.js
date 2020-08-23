87248d3526d9da99cf4c13e43633962d9f13dcc2
Chrispolanco <chrispolanco@sbcglobal.net>
2020-08-23 15:59:44 -0700

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -35,9 +35,9 @@ class Quotes extends Component {
                 <QuoteCard
                   key={quote.id}
                   quote={quote}
-                  upvoteQuote={e => this.handleUpvote(event, quote.id)}
-                  downvoteQuote={e => this.handleDownvote(event, quote.id)}
-                  removeQuote={e => this.handleRemoveQuote(event, quote.id)}
+                  upvoteQuote={event => this.handleUpvote(event, quote.id)}
+                  downvoteQuote={event => this.handleDownvote(event, quote.id)}
+                  removeQuote={event => this.handleRemoveQuote(event, quote.id)}
                 />
                 )}
             </div>
